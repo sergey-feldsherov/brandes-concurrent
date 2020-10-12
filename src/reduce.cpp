cff66b064f934bf7434ca62254d05f7b5f8001e7
Feldsherov Sergey <imnosuperman74@gmail.com>
2020-10-12 18:33:47 +0300

    Finished initial version of reduce program

--- src/reduce.cpp
+++ src/reduce.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cassert>
+#include <dirent.h>
+#include <vector>
+#include <unordered_map>
 
 int main(int argc, char** argv) {
 	assert(argc <= 2);
@@ -8,9 +12,90 @@ int main(int argc, char** argv) {
 	std::string indir;
 	if(argc == 1) {
 		indir = "./";
-		std::cout << "Looking for .txt files in current directory\n";
 	} else {
 		indir = argv[1];
-		std::cout << "Looking for .txt files in \"" << indir << "\"\n";
+		if(indir.back() != '/') {
+			indir.append("/");
+		}
 	}
+	std::cout << "Looking for .txt files in \"" << indir << "\"\n";
+
+	std::vector<std::string> filesVec;
+
+	DIR *dir;
+	struct dirent *ent;
+	if((dir = opendir(indir.c_str())) != NULL) {
+		while((ent = readdir(dir)) != NULL) {
+			std::string newFileName = ent->d_name;
+			std::string suffix = ".txt";
+			if(newFileName.length() >= 4 && newFileName.compare(newFileName.length() - suffix.length(), suffix.length(), suffix) == 0) {
+				filesVec.push_back(indir + ent->d_name);
+			}
+		}
+		closedir(dir);
+	} else {
+		std::cout << "Unable to open directory \"" << indir << "\"\n";
+		return -1;
+	}
+
+	if(filesVec.size() == 0) {
+		std::cout << "Found 0 .txt files\n";
+		return 0;
+	}
+	std::cout << "Found " << filesVec.size() << " .txt files:\n";
+	for(auto name: filesVec) {
+		std::cout << "\t" << name << "\n";
+	}
+
+	std::unordered_map<unsigned int, double> reducedScores;
+	for(auto name: filesVec) {
+		std::cout << "Reading file " << name << "\n";
+		std::ifstream ifs;
+		ifs.open(name);
+		while(ifs.peek() != EOF) {
+			char line[256];
+			ifs.getline(line, 256);
+			unsigned int vertex;
+			double score;
+			if(sscanf(line, "%u %lf", &vertex, &score) == 2) {
+                reducedScores[vertex] += score;
+            } else {
+                printf("\tInvalid line: %s\n", line);
+				ifs.close();
+                abort();
+            }
+		}
+		ifs.close();
+	}
+	std::cout << "Reading done\n";
+
+	std::cout << "Writing to \"reduced.txt\"\n";
+	std::ofstream ofs;
+	ofs.open("reduced.txt");
+	for(auto p: reducedScores) {
+		ofs << p.first << " " << p.second << "\n";
+	}
+	ofs.close();
+	std::cout << "Finished\n";
+
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
