2f88146ff1ce143b97184f4392c07e116d23bb73
Feldsherov Sergey <imnosuperman74@gmail.com>
2020-10-14 08:29:34 +0300

    Sync

--- src/errCalc.cpp
+++ src/errCalc.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <unordered_map>
+#include <unordered_set>
 #include <cassert>
 #include <string>
 #include <cmath> //fabs(), fmax(), sqrt()
@@ -9,8 +10,10 @@ unsigned long long currTimeNano();
 static int parse_opt(int key, char *arg, struct argp_state *state);
 
 struct Args {
-	std::string file1;
-	std::string file2;
+	std::string file1 = "./file1.txt";
+	std::string file2 = "./file2.txt";
+    std::string split1 = " ";
+    std::string split2 = " ";
 };
 
 Args my_args;
@@ -19,8 +22,10 @@ int main(int argc, char** argv) {
     auto t0 = currTimeNano();
 
     struct argp_option options[] = {
-        {  "file1",  -1, "FILE", 0,  "Path to first file" },
-        {  "file2",  -2, "FILE", 0, "Path to second file" },
+        {  "file1", -1, "FILE", 0,    "Path to first file" },
+        {  "file2", -2, "FILE", 0,   "Path to second file" },
+        { "split1", -3,  "STR", 0,  "Split for first file" },
+        { "split2", -4,  "STR", 0, "Split for second file" },
         { 0 }
     };
     struct argp argp = {options, parse_opt, 0, 0};
@@ -29,6 +34,7 @@ int main(int argc, char** argv) {
     auto timeRead1Start = currTimeNano();
 	printf("Reading file \"%s\".\n", my_args.file1.c_str());
 	std::unordered_map<unsigned int, double> data1;
+    std::unordered_set<unsigned int> vertices1;
     FILE* input = fopen(my_args.file1.c_str(), "r");
     if(input == NULL) {
         printf("Error while opening file \"%s\", aborting\n", my_args.file1.c_str());
@@ -38,23 +44,31 @@ int main(int argc, char** argv) {
     size_t len = 0;
     unsigned int vertex;
     double score;
+    unsigned int lineCount = 0;
     while(getline(&line, &len, input) != -1) {
-        if(sscanf(line, "%u %lf", &vertex, &score) == 2) {
-            data1[vertex] = score;
-        } else {
+        std::string tmp = line;
+        size_t splitPos = tmp.find(my_args.split1);
+        if(std::string::npos == splitPos) {
             printf("Invalid line: \"%s\" in file \"%s\", aborting\n", line, my_args.file1.c_str());
             abort();
         }
+        vertex = std::stoi(tmp.substr(0, splitPos));
+        score = std::stod(tmp.substr(splitPos + 1, tmp.length()));
+        //printf("From string \"%s\", %u %lf\n", tmp.c_str(), vertex, score);
+        data1[vertex] = score;
+        vertices1.insert(vertex);
+        lineCount++;
     }
     if(line) {
         free(line);
     }
     fclose(input);
-	printf("Done (%.2lf s)\nVertices: %lu\n", (currTimeNano() - timeRead1Start)*1e-9, data1.size());
+	printf("Done (%.2lf s)\nVertices: %lu\nLines in file: %u\n\n", (currTimeNano() - timeRead1Start)*1e-9, vertices1.size(), lineCount);
 
     auto timeRead2Start = currTimeNano();
 	printf("Reading file \"%s\".\n", my_args.file2.c_str());
 	std::unordered_map<unsigned int, double> data2;
+    std::unordered_set<unsigned int> vertices2;
     input = fopen(my_args.file2.c_str(), "r");
     if(input == NULL) {
         printf("Error while opening file \"%s\", aborting\n", my_args.file2.c_str());
@@ -62,21 +76,37 @@ int main(int argc, char** argv) {
     }
     line = NULL;
     len = 0;
+    lineCount = 0;
     while(getline(&line, &len, input) != -1) {
-        if(sscanf(line, "%u %lf", &vertex, &score) == 2) {
+        std::string tmp = line;
+        size_t splitPos = tmp.find(my_args.split2);
+        if(std::string::npos == splitPos) {
+            printf("Invalid line: \"%s\" in file \"%s\", aborting\n", line, my_args.file2.c_str());
+            abort();
+        }
+        vertex = std::stoi(tmp.substr(0, splitPos));
+        score = std::stod(tmp.substr(splitPos + 1, tmp.length()));
+        data2[vertex] = score;
+        lineCount++;
+        vertices2.insert(vertex);
+        //printf("From string \"%s\", %u %lf\n", tmp.c_str(), vertex, score);
+        /*if(sscanf(line, "%u %lf", &vertex, &score) == 2) {
             data2[vertex] = score;
         } else {
             printf("Invalid line: \"%s\" in file \"%s\", aborting\n", line, my_args.file2.c_str());
             abort();
-        }
+        }*/
     }
     if(line) {
         free(line);
     }
     fclose(input);
-	printf("Done (%.2lf s)\nVertices: %lu\n", (currTimeNano() - timeRead2Start)*1e-9, data2.size());
+	printf("Done (%.2lf s)\nVertices: %lu\nLines in file: %u\n\n", (currTimeNano() - timeRead2Start)*1e-9, vertices2.size(), lineCount);
 
-    assert(data1.size() == data2.size());
+    if(data1.size() != data2.size()) {
+        printf("Warning: scores may be for different graphs (vertice set sizes are not equal)\n");
+    }
+    //assert(data1.size() == data2.size());
 
 	double sumErr = 0.;
     double sumSquareErr = 0.;
@@ -91,11 +121,13 @@ int main(int argc, char** argv) {
     }
 
     printf("\n");
-	printf("L1:   \t%lf (sum of absolute errors)\n", sumErr);
-	printf("L2:   \t%lf (Euclidean norm of error vector)\n", sqrt(sumSquareErr));
-	printf("Linf: \t%lf (max absolute error)\n", maxErr);
+	printf("L1:   %lf (sum of absolute errors)\n", sumErr);
+	printf("L2:   %lf (Euclidean norm of error vector)\n", sqrt(sumSquareErr));
+	printf("Linf: %lf (max absolute error)\n", maxErr);
+    printf("E:    %lf (average error)\n", sumErr / fmax(data1.size(), data2.size()));
+    printf("RMSE: %lf (root mean square error)\n", sqrt(sumSquareErr / fmax(data1.size(), data2.size())));
 
-    printf("\nFinished (%.2lf s)\n", (currTimeNano() - timeRead1Start)*1e-9);
+    printf("\nFinished (%.2lf s)\n", (currTimeNano() - t0)*1e-9);
     return 0;
 }
 
@@ -105,6 +137,10 @@ static int parse_opt(int key, char *arg, struct argp_state *state) {
         my_args.file1 = arg;
     } else if(key == -2) {
         my_args.file2 = arg;
+    } else if(key == -3) {
+        my_args.split1 = arg;
+    } else if(key == -4) {
+        my_args.split2 = arg;
     }
 
     return 0;
