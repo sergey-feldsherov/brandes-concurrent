71513b19ec0b28cf9f7b7d51b1902b6d221121e1
sergey-feldsherov <imnosuperman74@gmail.com>
2022-10-30 00:54:30 +0300

    Fix compilation warnings & correct code style

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 globalArgs_t globalArgs;
 
-static int parse_opt(int key, char *arg, struct argp_state *state);
+static int parse_opt(int key, char *arg, struct argp_state *);
 
 int main(int argc, char **argv) {
 
@@ -15,18 +15,18 @@ int main(int argc, char **argv) {
 
     //create stuff for argp
     struct argp_option options[] = {
-        { "debug",      'd',      0, 0,                                                         "Debugging information display" },
-        { "undirected", 'u',      0, 0,                                                             "Treat graph as undirected" },
-        { "input",      'i', "FILE", 0,                                                                            "Input file" },
-        { "output",     'o',  "DIR", 0,                                                                      "Output directory" },
-        { "threads",    't',  "NUM", 0,           "Number of threads: <=0 - serial, >= 1 -> concurrent with threads allocation" },
-        { "start",      's',   "ID", 0,                      "Starting vertex id for calculating contributions, first included" },
-        { "finish",     'f',   "ID", 0,                  "Finishing vertex id for calculating contributions, last not included" },
-        { "autosave",   'a',  "MIN", 0,                 "Time interval in minutes between part-way saves of calculation result" },
-        { "norenum",    'n',      0, 0, "Do not renumerate graph vertices (graph vertices are numbered 0..N-1 and are present)" },
-        { 0 }
+        {      "debug", 'd',      0, 0,                                                         "Debugging information display", 0 },
+        { "undirected", 'u',      0, 0,                                                             "Treat graph as undirected", 0 },
+        {      "input", 'i', "FILE", 0,                                                                            "Input file", 0 },
+        {     "output", 'o',  "DIR", 0,                                                                      "Output directory", 0 },
+        {    "threads", 't',  "NUM", 0,           "Number of threads: <=0 - serial, >= 1 -> concurrent with threads allocation", 0 },
+        {      "start", 's',   "ID", 0,                      "Starting vertex id for calculating contributions, first included", 0 },
+        {     "finish", 'f',   "ID", 0,                  "Finishing vertex id for calculating contributions, last not included", 0 },
+        {   "autosave", 'a',  "MIN", 0,                 "Time interval in minutes between part-way saves of calculation result", 0 },
+        {    "norenum", 'n',      0, 0, "Do not renumerate graph vertices (graph vertices are numbered 0..N-1 and are present)", 0 },
+        {            0,   0,      0, 0,                                                                                       0, 0 }
     };
-    struct argp argp = { options, parse_opt, 0, 0 };
+    struct argp argp = { options, parse_opt, 0, 0, 0, 0, 0 };
 
     //parse args and put stuff into globalArgs
     argp_parse(&argp, argc, argv, 0, 0, 0);
@@ -57,7 +57,7 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-static int parse_opt(int key, char *arg, struct argp_state *state) {
+static int parse_opt(int key, char *arg, struct argp_state *) {
     if(key == 'd') {
         globalArgs.debug = true;
     } else if(key == 'u') {
